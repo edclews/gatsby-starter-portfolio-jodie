ecefc6841523f9bf8d8c2d26ebf6f29d37aabe85
Ed Clews <ed@edclews.com>
2020-05-21 11:12:31 +0100

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,8 +17,9 @@ const About = () => {
       <AnimatedBox style={pageAnimation} py={[6, 6, 6, 8]} px={[6, 6, 8, 6, 8, 13]}>
         <h1>Hi. I'm Ed.</h1>
         <p>
-          You can visit my <a href="https://www.lekoarts.de/en">website</a> or my other{' '}
-          <a href="https://gatsby-starter-portfolio.netlify.com">Gatsby projects</a>.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed imperdiet tellus, ac cursus velit. Nunc mattis ante vel urna vestibulum, vitae pulvinar tellus aliquam. Donec tincidunt a erat sit amet imperdiet. Nam congue purus eu ante viverra, a malesuada dui lacinia. Morbi eu lobortis ante, a cursus arcu. Sed dolor augue, vulputate vel lorem sed, rhoncus tincidunt purus. Nam at congue odio. </p>
+
+          <p>Nunc dictum turpis sapien, a pretium quam venenatis iaculis. Donec eu condimentum massa. Cras interdum felis elit. Suspendisse in pharetra velit, nec consectetur diam. Integer nisi mauris, pulvinar quis iaculis ac, luctus vitae arcu. In risus nisl, dapibus sit amet fermentum vel, pellentesque quis mi. Nam tempus imperdiet sapien ullamcorper accumsan. In at mauris bibendum, luctus nisi et, ultrices nisl.
         </p>
       </AnimatedBox>
     </Layout>
