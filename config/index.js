28620ee2054ae9f722beb4bffb142897dbc55ea7
Ed Clews <ed@edclews.com>
2020-08-31 20:21:46 +0100

    Confihg amend

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   siteTitleAlt: 'Ed Clews Portfolio', // Alternative Site title for SEO
   siteTitleShort: 'EdClews', // short_name for manifest
   siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://infallible-brown-0a508b.netlify.app', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
   siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
